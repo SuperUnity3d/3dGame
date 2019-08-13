9f06989bfee62823f72b8bbfd87b1d370ec095d1
SUPERGAMESGROUP <SUPERGAMESGROUP@protonmail.com>
2019-08-13 11:46:33 +0300

    + MouseLook + FPSInput

---
+++ SpacePolice/Assets/Scripts/Spin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (speed == 6.0f)
+        { //если скорость равна 6
+            transform.Rotate(0, speed, 0);
+        }
+        else
+        { //иначе куб поворачивается в другую сторону
+            transform.Rotate(0, -speed, 0);
+        }
+
+    }
+}
